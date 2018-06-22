3b8cda4e49634e28535bb63b52a2ebfcb52e5e48
Nate Miller <natekmiller96@gmail.com>
2018-06-22 19:04:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,11 @@ def batch_badge_creator(array)
   end
   return batch
 end
+def assign_rooms(array)
+  room = 1
+  batch = []
+  array.each do |index|
+    batch << "Hello, #{index} You'll be assigned to room #{room}"
+    room +=
+  end
+  return batch
